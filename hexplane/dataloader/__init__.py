5721c7491dbe4b2a35f8376056526c0d264d5347
meush <116996562+dsawebappayush@users.noreply.github.com>
2024-02-09 01:03:07 +0530

    Update __init__.py

--- hexplane/dataloader/__init__.py
+++ hexplane/dataloader/__init__.py
@@ -1,7 +1,7 @@
 from .dnerf_dataset import DNerfDataset
 from .neural_3D_dataset_NDC import Neural3D_NDC_Dataset
-from .phototourism_dataset.py import PhotoTourismDataset
-
+from .phototourism_dataset import PhotoTourismDataset
+from .llff import LLFFDataset
 def get_train_dataset(cfg, is_stack=False):
     if cfg.data.dataset_name == "dnerf":
         train_dataset = DNerfDataset(
@@ -33,17 +33,20 @@ def get_train_dataset(cfg, is_stack=False):
             eval_index=cfg.data.nv3d_ndc_eval_index,
             sphere_scale=cfg.data.nv3d_ndc_sphere_scale,
         )
-    elif cfg.data.dataset_name=="photo":
-        train_dataset=PhotoTourismDataset(
-            cfg.data.datadir,
-            split = "train",
-            batch_size = cfg.data.batch_size
-            contraction=cfg.data.contraction,
-            ndc=cfg.data.ndc,
-            scene_bbox=cfg.data.scene_bbox,
-            global_translation=cfg.data.global_translation,
-            global_scale=cfg.data.global_scale,
-            downsample=cfg.data.downsampl,
+    elif cfg.data.dataset_name=="llff":
+         train_dataset=LLFFDataset(
+         cfg.data.datadir,
+         split="train",
+         hold_every=8,
+         is_stack=is_stack,
+         downsample=4,
+         blender2opencv=np.eye(4),
+         white_bg=False,
+         near_far=[0.0,1.0],
+         scene_bbox=torch.tensor([[-1.5, -1.67, -1.0], [1.5, 1.67, 1.0]]),
+         centre=torch.mean(self.scene_bbox, dim=0).float().view(1, 1, 3),
+         invradius=1.0 / (self.scene_bbox[1] - self.center).float().view(1, 1, 3),
+
         )
     else:
         raise NotImplementedError("No such dataset")
@@ -81,20 +84,20 @@ def get_test_dataset(cfg, is_stack=True):
             eval_index=cfg.data.nv3d_ndc_eval_index,
             sphere_scale=cfg.data.nv3d_ndc_sphere_scale,
         )
-    elif cfg.data.dataset_name=="iphone":
-        test_dataset=PhotoTourismDataset(
+    elif cfg.data.dataset_name=="llff":
+        test_dataset=LLFFDataset(
 
-            cfg.data.datadir,
-            split = "test",
-            batch_size = cfg.data.batch_size
-            contraction=cfg.data.contraction,
-            ndc=cfg.data.ndc,
-            scene_bbox=cfg.data.scene_bbox,
-            global_translation=cfg.data.global_translation,
-            global_scale=cfg.data.global_scale,
-            downsample=cfg.data.downsampl,
-         
-            
+         cfg.data.datadir,
+         split="test",
+         hold_every=8,
+         is_stack=is_stack,
+         downsample=4,
+         blender2opencv=np.eye(4),
+         white_bg=False,
+         near_far=[0.0,1.0],
+         scene_bbox=torch.tensor([[-1.5, -1.67, -1.0], [1.5, 1.67, 1.0]]),
+         centre=torch.mean(self.scene_bbox, dim=0).float().view(1, 1, 3),
+         invradius=1.0 / (self.scene_bbox[1] - self.center).float().view(1, 1, 3),
 
 
             
